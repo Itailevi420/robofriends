fff5e8d9e91470830aa0414405fa93bf7d15b62e
Itai Levi <itailevi420@gmail.com>
2020-09-08 13:46:08 +0300

    Creating necessary files for redux integration

--- src/container/App.js
+++ src/container/App.js
@@ -1,50 +1,57 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../component/CardList';
 import SearchBox from '../component/SearchBox';
 import './App.css';
 import Scroll from '../component/Scroll';
+import { setSearchField } from '../action';
+
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchField,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+  };
+};
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       robots: [],
-      searchfield: ''
-    }
+    };
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response=> response.json())
-      .then(users => this.setState({robots: users}));
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value }) 
-  }
+  render() {
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
+    const filterdRobo = robots.filter((robot) => {
+      return (
+        robot.name.toLowerCase().includes(searchField.toLowerCase()) ||
+        robot.email.toLowerCase().includes(searchField.toLowerCase())
+      );
+    });
 
-  // trying to search all data & not just 'name';
-
-  // render(){
-  //   const filterdRobo = this.state.robots.filter(robot=> {
-  //     return Object.values(robot).some(val => val.includes(this.state.searchfield.toLowerCase()))
-  // })
-  render(){
-    const {robots, searchfield} = this.state;
-    const filterdRobo = robots.filter(robot=> {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase()) || robot.email.toLowerCase().includes(searchfield.toLowerCase());
-    })
-        
     return (
-      <div className='tc'>
-        <h1 className='f1'>My friends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
+      <div className="tc">
+        <h1 className="f1">My friends</h1>
+        <SearchBox searchChange={onSearchChange} />
         <Scroll>
           <CardList robots={filterdRobo} />
         </Scroll>
       </div>
     );
   }
-};
+}
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
